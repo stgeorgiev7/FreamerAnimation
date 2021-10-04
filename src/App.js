1a4c3cff08cecc20b11e912c73159975d9b14e9f
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-04 16:13:34 +0300

    added animations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { motion } from "framer-motion";
+
 
 function App() {
   return (
     <div className="App">
-      <form>
+      <motion.form 
+      initial={{x:'-100vw'}}
+      animate={{x:0, translateX:10}}>
         <h1>Create An Account</h1>
         <label>
           Email:
@@ -14,7 +18,7 @@ function App() {
           <input name="password" type="password" required />
         </label>
         <button>Sign Up!</button>
-      </form>
+      </motion.form>
     </div>
   );
 }
